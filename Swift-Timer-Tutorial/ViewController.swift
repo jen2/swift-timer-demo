d898de1a2e3353ff076e29c56cad735e66e0e413
jen2 <sipila.jen@gmail.com>
2016-12-03 12:57:24 -0500

    Completed tutorial

--- Swift-Timer-Tutorial/ViewController.swift
+++ Swift-Timer-Tutorial/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var timerLabel: UILabel!
 
+    var seconds = 0
+    var timer = Timer()
+    var isTimerRunning = false
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        if isTimerRunning == false {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+            
+            isTimerRunning = true
+        }
+    }
+    
+    @IBAction func pauseButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        seconds = 0
+        timerLabel.text = timeString(time: TimeInterval(seconds))
+        
+        isTimerRunning = false
+    }
+    
+    
+    func updateTimer(){
+        
+        seconds += 1
+        timerLabel.text = timeString(time: TimeInterval(seconds))
+        
+    }
+    
+    func timeString(time:TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
