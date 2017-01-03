de9c8ea5cf7f163cc20ff8ab60f9a181e4dc8c47
jen2 <sipila.jen@gmail.com>
2017-01-03 10:22:30 -0500

    Refactored code, added pause/resume functionality, added notes about counting down instead of up

--- Swift-Timer-Tutorial/ViewController.swift
+++ Swift-Timer-Tutorial/ViewController.swift
@@ -12,25 +12,38 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var timerLabel: UILabel!
 
-    var seconds = 0
+    var seconds = 0 // To make this time count down set an amount of seconds that is greater than 0 here.
     var timer = Timer()
     var isTimerRunning = false
+    var pauseTapped = true
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
         if isTimerRunning == false {
-            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
-            
-            isTimerRunning = true
+            runTimer()
         }
     }
     
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+        
+        isTimerRunning = true
+    }
+    
+    //Resume functionality for timer counting up from 0.
     @IBAction func pauseButtonTapped(_ sender: UIButton) {
-        timer.invalidate()
+        if pauseTapped == true {
+            timer.invalidate()
+            pauseTapped = false
+        }
+        if pauseTapped == false {
+            runTimer()
+            pauseTapped = true
+        }
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
         timer.invalidate()
-        seconds = 0
+        seconds = 0 //To keep that initial starting value of seconds change this to the same as above.
         timerLabel.text = timeString(time: TimeInterval(seconds))
         
         isTimerRunning = false
@@ -38,7 +51,7 @@ class ViewController: UIViewController {
     
     func updateTimer(){
         
-        seconds += 1
+        seconds += 1 //Now the timer needs to count down so change "seconds += 1" to "seconds -= 1".
         timerLabel.text = timeString(time: TimeInterval(seconds))
         
     }
