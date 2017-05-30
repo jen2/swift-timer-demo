cb00e70778108975a398ba0364db98c6a89dbe22
jen2 <sipila.jen@gmail.com>
2017-05-29 20:43:31 -0400

    Bug fix on start button implementation

--- Swift-Timer-Tutorial/ViewController.swift
+++ Swift-Timer-Tutorial/ViewController.swift
@@ -13,9 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var pauseButton: UIButton!
     @IBOutlet weak var timerLabel: UILabel!
+//    @IBOutlet weak var labelButton: UIButton!
 
+    
     var seconds = 60
     var timer = Timer()
+ 
     var isTimerRunning = false
     var resumeTapped = false
     
@@ -35,11 +38,13 @@ class ViewController: UIViewController {
     @IBAction func pauseButtonTapped(_ sender: UIButton) {
         if self.resumeTapped == false {
             timer.invalidate()
+            isTimerRunning = false
             self.resumeTapped = true
             self.pauseButton.setTitle("Resume",for: .normal)
         } else {
             runTimer()
             self.resumeTapped = false
+            isTimerRunning = true
             self.pauseButton.setTitle("Pause",for: .normal)
         }
     }
@@ -50,15 +55,18 @@ class ViewController: UIViewController {
         timerLabel.text = timeString(time: TimeInterval(seconds))
         isTimerRunning = false
         pauseButton.isEnabled = false
+        startButton.isEnabled = true
     }
     
-    func updateTimer(){
+    func updateTimer() {
         if seconds < 1 {
             timer.invalidate()
             //Send alert to indicate time's up.
         } else {
             seconds -= 1
             timerLabel.text = timeString(time: TimeInterval(seconds))
+            timerLabel.text = String(seconds)
+//            labelButton.setTitle(timeString(time: TimeInterval(seconds)), for: UIControlState.normal)
         }
     }
     
